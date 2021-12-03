6963d9a1e6a65a71cb3f10b4bd603f429bf47c04
kimocoder <christian@aircrack-ng.org>
2021-12-03 14:33:16 +0100

    Fix minor compiling issues

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -816,14 +816,14 @@ int f2fs_fill_dentries(struct dir_context *ctx, struct f2fs_dentry_ptr *d,
 
 		if (unlikely(bit_pos > d->max ||
 				le16_to_cpu(de->name_len) > F2FS_NAME_LEN)) {
-			f2fs_msg(F2FS_I_SB(d->inode)->sb, KERN_WARNING,
-				"%s: corrupted namelen=%d, run fsck to fix.",
-				__func__, le16_to_cpu(de->name_len));
+			//f2fs_msg(F2FS_I_SB(d->inode)->sb, KERN_WARNING,
+				//"%s: corrupted namelen=%d, run fsck to fix.",
+				//__func__, le16_to_cpu(de->name_len));
 			set_sbi_flag(F2FS_I_SB(d->inode)->sb->s_fs_info, SBI_NEED_FSCK);
 			return -EINVAL;
 		}
 
-		if (f2fs_encrypted_inode(d->inode)) {
+		/*if (f2fs_encrypted_inode(d->inode)) {
 			int save_len = fstr->len;
 
 			err = fscrypt_fname_disk_to_usr(d->inode,
@@ -834,7 +834,7 @@ int f2fs_fill_dentries(struct dir_context *ctx, struct f2fs_dentry_ptr *d,
 
 			de_name = *fstr;
 			fstr->len = save_len;
-		}
+		}*/
 
 		if (!dir_emit(ctx, de_name.name, de_name.len,
 					le32_to_cpu(de->ino), d_type)) {
